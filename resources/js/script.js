8d9b40eb41cabd6b0ef927ed92cc54d6a3b8b187
Aaqib Parvez <aaqib29292@gmail.com>
2016-08-18 02:00:45 +0530

    completed stuffs section

---
+++ resources/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+  // FitVids plugin
+
+
+    // Target your .container, .wrapper, .post, etc.
+    $("figure.vimeo").fitVids();
+    console.log("video");
+
+
+})
